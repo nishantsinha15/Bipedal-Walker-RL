1b1ae05bb3023e48a4249dad075c08f2e0586865
Nishant Sinha <nishant15066@iiitd.ac.in>
2018-11-17 15:18:54 +0530

    Added stacked frames for a state.

--- Atari Testing/DQN.py
+++ Atari Testing/DQN.py
@@ -31,8 +31,6 @@ class DeepQAgent:
         self.action_space = action_space
 
     def get_action_from_prediction(self, predict):
-        print(len(predict[0]))
-        print(predict)
         return self.action_space[np.argmax(predict[0])]
 
     def _build_model(self):
@@ -73,40 +71,50 @@ class DeepQAgent:
 
 
 if __name__ == "__main__":
-    env = gym.make('BipedalWalker-v2')
+    env = gym.make('Breakout-ram-v0')
     state_size = env.observation_space.shape[0]
-    actions_space = get_actions()
-    action_size = len(actions_space)
-    agent = DeepQAgent(state_size, actions_space)
-    agent2 = DeepQAgent(state_size, actions_space)
-    done = False
-    batch_size = 32
-    c = 0
-    recent_average = deque(maxlen=100)
     for e in range(EPISODES):
         state = env.reset()
-        state = np.reshape(state, [1, state_size])
-        total_reward = 0
         for time in range(500):
-            c += 1
-            # env.render()
-            action = agent.act(state)
+            env.render()
+            action = env.action_space.sample()
             next_state, reward, done, _ = env.step(action)
-            total_reward += reward
-            next_state = np.reshape(next_state, [1, state_size])
-            agent.remember(state, action, reward, next_state, done)
+            print("reward = ", reward)
+            print("next state = ", (next_state))
             state = next_state
-            if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, total_reward, agent.epsilon))
-                recent_average.append(total_reward)
-                print("c = ", c, " Recent Average = ", sum(recent_average) / len(recent_average))
-                break
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size, agent2)
-            if c >= 1000:
-                print('updating model')
-                c = 0
-                agent2.model.set_weights(agent.model.get_weights())
-        if e % 50 == 0:
-            agent.save("Bipedal-dqn-nishant-v2.h5")
+
+    # actions_space = get_actions()
+    # action_size = len(actions_space)
+    # agent = DeepQAgent(state_size, actions_space)
+    # agent2 = DeepQAgent(state_size, actions_space)
+    # done = False
+    # batch_size = 32
+    # c = 0
+    # recent_average = deque(maxlen=100)
+    # for e in range(EPISODES):
+    #     state = env.reset()
+    #     state = np.reshape(state, [1, state_size])
+    #     total_reward = 0
+    #     for time in range(500):
+    #         c += 1
+    #         # env.render()
+    #         action = agent.act(state)
+    #         next_state, reward, done, _ = env.step(action)
+    #         total_reward += reward
+    #         next_state = np.reshape(next_state, [1, state_size])
+    #         agent.remember(state, action, reward, next_state, done)
+    #         state = next_state
+    #         if done:
+    #             print("episode: {}/{}, score: {}, e: {:.2}"
+    #                   .format(e, EPISODES, total_reward, agent.epsilon))
+    #             recent_average.append(total_reward)
+    #             print("c = ", c, " Recent Average = ", sum(recent_average) / len(recent_average))
+    #             break
+    #         if len(agent.memory) > batch_size:
+    #             agent.replay(batch_size, agent2)
+    #         if c >= 1000:
+    #             print('updating model')
+    #             c = 0
+    #             agent2.model.set_weights(agent.model.get_weights())
+    #     if e % 50 == 0:
+    #         agent.save("Bipedal-dqn-nishant-v2.h5")
--- Beta/DQN_stacked_frames.py
+++ Beta/DQN_stacked_frames.py
@@ -6,8 +6,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import sgd
 from itertools import product as possibleIterations
-
-EPISODES = 1000
+import matplotlib.pyplot as plt
 
 
 def get_actions():
@@ -17,11 +16,41 @@ def get_actions():
     return possibleActions
 
 
+EPISODES = 1000
+actions_space = get_actions()
+
+def plot(data):
+    x=[]
+    y=[]
+    for i,j in data:
+        x.append(i)
+        y.append(j)
+    plt.plot(x,y)
+    plt.savefig('temp.png')
+
+class State:
+    def __init__(self, states, actions):
+        self.states = states# 4 sattes
+        self.actions = actions # 3 actions
+
+    def get_input_layer(self):
+        ret = []
+        for i in range(3):
+            ret = ret + list(self.states[i][0])
+            ret.append(actions_space.tolist().index(self.actions[i].tolist()))
+        ret = ret + list(self.states[3][0])
+        ret = np.array(ret)
+        ret = np.reshape(ret, [1, 24*4 + 3])
+
+        return ret
+
+
+
 class DeepQAgent:
     def __init__(self, state_size, action_space):
         self.state_size = state_size
         self.action_size = 81
-        self.memory = deque(maxlen=200000)
+        self.memory = deque(maxlen=20000)
         self.gamma = 0.95
         self.epsilon = 1.0
         self.epsilon_min = 0.01
@@ -31,14 +60,12 @@ class DeepQAgent:
         self.action_space = action_space
 
     def get_action_from_prediction(self, predict):
-        print(len(predict[0]))
-        print(predict)
         return self.action_space[np.argmax(predict[0])]
 
     def _build_model(self):
         model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))  # changed layer count from 24
-        model.add(Dense(48, activation='relu'))
+        model.add(Dense(100, input_dim=self.state_size, activation='relu'))  # changed layer count from 24
+        model.add(Dense(80, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))  # changed this from linear
         model.compile(loss='mse', optimizer=sgd(lr=self.learning_rate))
         return model
@@ -50,7 +77,7 @@ class DeepQAgent:
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return self.action_space[np.random.choice([i for i in range(len(self.action_space))])]
-        act_values = self.model.predict(state)  # what does this return
+        act_values = self.model.predict(state.get_input_layer())  # what does this return
         return self.get_action_from_prediction(act_values)
 
     def replay(self, batch_size, agent2):
@@ -58,10 +85,10 @@ class DeepQAgent:
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-                target = (reward + self.gamma * np.amax(agent2.model.predict(next_state)[0]))  # Returns q-values
-            target_f = agent2.model.predict(state)
+                target = (reward + self.gamma * np.amax(agent2.model.predict(next_state.get_input_layer())[0]))  # Returns q-values
+            target_f = agent2.model.predict(state.get_input_layer())
             target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+            self.model.fit(state.get_input_layer(), target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -73,12 +100,12 @@ class DeepQAgent:
 
 
 if __name__ == "__main__":
+    eVSs = deque(maxlen=1000)
     env = gym.make('BipedalWalker-v2')
     state_size = env.observation_space.shape[0]
-    actions_space = get_actions()
     action_size = len(actions_space)
-    agent = DeepQAgent(state_size, actions_space)
-    agent2 = DeepQAgent(state_size, actions_space)
+    agent = DeepQAgent(state_size*4 + 3, actions_space)
+    agent2 = DeepQAgent(state_size*4 + 3, actions_space)
     done = False
     batch_size = 32
     c = 0
@@ -87,26 +114,48 @@ if __name__ == "__main__":
         state = env.reset()
         state = np.reshape(state, [1, state_size])
         total_reward = 0
+        prev_state = State([state for i in range(4)], [np.array([-1.0, 0.0, 1.0, 1.0]) for i in range(3)])
+        curr_state = State([state for i in range(4)], [np.array([-1.0, 0.0, 1.0, 1.0]) for i in range(3)])
+        my_state = deque(maxlen=4)
+        my_actions = deque(maxlen=3)
+        my_state.append(state)
+        flag = True
         for time in range(500):
             c += 1
             # env.render()
-            action = agent.act(state)
+            action = agent.act(curr_state)
+            my_actions.append(action)
+
             next_state, reward, done, _ = env.step(action)
             total_reward += reward
             next_state = np.reshape(next_state, [1, state_size])
-            agent.remember(state, action, reward, next_state, done)
             state = next_state
+            my_state.append(state)
+
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
                       .format(e, EPISODES, total_reward, agent.epsilon))
                 recent_average.append(total_reward)
-                print("c = ", c, " Recent Average = ", sum(recent_average) / len(recent_average))
+                av = sum(recent_average) / len(recent_average)
+                print("c = ", c, " Recent Average = ", av)
+                eVSs.append((e+1,av))
                 break
+
+            if len(my_state) == 4:
+                curr_state = State(my_state, my_actions)
+                if flag:
+                    prev_state = curr_state
+                    flag = False
+                agent.remember(prev_state, action, reward, curr_state, done)
+                prev_state = curr_state
+
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size, agent2)
             if c >= 1000:
                 print('updating model')
                 c = 0
                 agent2.model.set_weights(agent.model.get_weights())
-        if e % 50 == 0:
+        if e % 1 == 0:
             agent.save("Bipedal-dqn-nishant-v2.h5")
+            plot(eVSs)
+
