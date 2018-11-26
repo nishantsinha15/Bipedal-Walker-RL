329fbfad98f7cc23bb26a14ef8df007687cad182
saurabh kumar <skwow@users.noreply.github.com>
2018-11-26 17:52:05 +0530

    actor critic and keras RL

---
+++ Beta/AIProject-Explore/AIProjectKRL/temp.py
@@ -0,0 +1,418 @@
+from __future__ import division
+import numpy as np
+import gym
+
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Flatten, Input, Concatenate
+from keras.optimizers import Adam
+
+#from rl.agents import DDPGAgent
+from rl.memory import SequentialMemory
+from rl.random import OrnsteinUhlenbeckProcess
+
+from collections import deque
+import os
+import warnings
+import random
+import keras.backend as K
+import keras.optimizers as optimizers
+
+from rl.core import Agent
+from rl.random import OrnsteinUhlenbeckProcess
+from rl.util import *
+
+
+def mean_q(y_true, y_pred):
+    return K.mean(K.max(y_pred, axis=-1))
+
+
+# Deep DPG as described by Lillicrap et al. (2015)
+# http://arxiv.org/pdf/1509.02971v2.pdf
+# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.646.4324&rep=rep1&type=pdf
+class DDPGAgent(Agent):
+    """Write me
+    """
+    def __init__(self,env,  nb_actions, actor, critic, critic_action_input, memory,e = 1, steps = 500000,
+                 gamma=.99, batch_size=32, nb_steps_warmup_critic=1000, nb_steps_warmup_actor=1000,
+                 train_interval=1, memory_interval=1, delta_range=None, delta_clip=np.inf,
+                 random_process=None, custom_model_objects={}, target_model_update=.001, **kwargs):
+        if hasattr(actor.output, '__len__') and len(actor.output) > 1:
+            raise ValueError('Actor "{}" has more than one output. DDPG expects an actor that has a single output.'.format(actor))
+        if hasattr(critic.output, '__len__') and len(critic.output) > 1:
+            raise ValueError('Critic "{}" has more than one output. DDPG expects a critic that has a single output.'.format(critic))
+        if critic_action_input not in critic.input:
+            raise ValueError('Critic "{}" does not have designated action input "{}".'.format(critic, critic_action_input))
+        if not hasattr(critic.input, '__len__') or len(critic.input) < 2:
+            raise ValueError('Critic "{}" does not have enough inputs. The critic must have at exactly two inputs, one for the action and one for the observation.'.format(critic))
+
+        super(DDPGAgent, self).__init__(**kwargs)
+
+        # Soft vs hard target model updates.
+        if target_model_update < 0:
+            raise ValueError('`target_model_update` must be >= 0.')
+        elif target_model_update >= 1:
+            # Hard update every `target_model_update` steps.
+            target_model_update = int(target_model_update)
+        else:
+            # Soft update with `(1 - target_model_update) * old + target_model_update * new`.
+            target_model_update = float(target_model_update)
+
+        if delta_range is not None:
+            warnings.warn('`delta_range` is deprecated. Please use `delta_clip` instead, which takes a single scalar. For now we\'re falling back to `delta_range[1] = {}`'.format(delta_range[1]))
+            delta_clip = delta_range[1]
+
+        # Parameters.
+        self.env = env
+        self.e = e
+        self.steps = steps
+        self.currentStep =1
+        self.nb_actions = nb_actions
+        self.nb_steps_warmup_actor = nb_steps_warmup_actor
+        self.nb_steps_warmup_critic = nb_steps_warmup_critic
+        self.random_process = random_process
+        self.delta_clip = delta_clip
+        self.gamma = gamma
+        self.target_model_update = target_model_update
+        self.batch_size = batch_size
+        self.train_interval = train_interval
+        self.memory_interval = memory_interval
+        self.custom_model_objects = custom_model_objects
+
+        # Related objects.
+        self.actor = actor
+        self.critic = critic
+        self.critic_action_input = critic_action_input
+        self.critic_action_input_idx = self.critic.input.index(critic_action_input)
+        self.memory = memory
+
+        # State.
+        self.compiled = False
+        self.reset_states()
+
+    @property
+    def uses_learning_phase(self):
+        return self.actor.uses_learning_phase or self.critic.uses_learning_phase
+
+    def compile(self, optimizer, metrics=[]):
+        metrics += [mean_q]
+
+        if type(optimizer) in (list, tuple):
+            if len(optimizer) != 2:
+                raise ValueError('More than two optimizers provided. Please only provide a maximum of two optimizers, the first one for the actor and the second one for the critic.')
+            actor_optimizer, critic_optimizer = optimizer
+        else:
+            actor_optimizer = optimizer
+            critic_optimizer = clone_optimizer(optimizer)
+        if type(actor_optimizer) is str:
+            actor_optimizer = optimizers.get(actor_optimizer)
+        if type(critic_optimizer) is str:
+            critic_optimizer = optimizers.get(critic_optimizer)
+        assert actor_optimizer != critic_optimizer
+
+        if len(metrics) == 2 and hasattr(metrics[0], '__len__') and hasattr(metrics[1], '__len__'):
+            actor_metrics, critic_metrics = metrics
+        else:
+            actor_metrics = critic_metrics = metrics
+
+        def clipped_error(y_true, y_pred):
+            return K.mean(huber_loss(y_true, y_pred, self.delta_clip), axis=-1)
+
+        # Compile target networks. We only use them in feed-forward mode, hence we can pass any
+        # optimizer and loss since we never use it anyway.
+        self.target_actor = clone_model(self.actor, self.custom_model_objects)
+        self.target_actor.compile(optimizer='sgd', loss='mse')
+        self.target_critic = clone_model(self.critic, self.custom_model_objects)
+        self.target_critic.compile(optimizer='sgd', loss='mse')
+
+        # We also compile the actor. We never optimize the actor using Keras but instead compute
+        # the policy gradient ourselves. However, we need the actor in feed-forward mode, hence
+        # we also compile it with any optimzer and
+        self.actor.compile(optimizer='sgd', loss='mse')
+
+        # Compile the critic.
+        if self.target_model_update < 1.:
+            # We use the `AdditionalUpdatesOptimizer` to efficiently soft-update the target model.
+            critic_updates = get_soft_target_model_updates(self.target_critic, self.critic, self.target_model_update)
+            critic_optimizer = AdditionalUpdatesOptimizer(critic_optimizer, critic_updates)
+        self.critic.compile(optimizer=critic_optimizer, loss=clipped_error, metrics=critic_metrics)
+
+        # Combine actor and critic so that we can get the policy gradient.
+        # Assuming critic's state inputs are the same as actor's.
+        combined_inputs = []
+        state_inputs = []
+        for i in self.critic.input:
+            if i == self.critic_action_input:
+                combined_inputs.append([])
+            else:
+                combined_inputs.append(i)
+                state_inputs.append(i)
+        combined_inputs[self.critic_action_input_idx] = self.actor(state_inputs)
+
+        combined_output = self.critic(combined_inputs)
+
+        updates = actor_optimizer.get_updates(
+            params=self.actor.trainable_weights, loss=-K.mean(combined_output))
+        if self.target_model_update < 1.:
+            # Include soft target model updates.
+            updates += get_soft_target_model_updates(self.target_actor, self.actor, self.target_model_update)
+        updates += self.actor.updates  # include other updates of the actor, e.g. for BN
+
+        # Finally, combine it all into a callable function.
+        if K.backend() == 'tensorflow':
+            self.actor_train_fn = K.function(state_inputs + [K.learning_phase()],
+                                             [self.actor(state_inputs)], updates=updates)
+        else:
+            if self.uses_learning_phase:
+                state_inputs += [K.learning_phase()]
+            self.actor_train_fn = K.function(state_inputs, [self.actor(state_inputs)], updates=updates)
+        self.actor_optimizer = actor_optimizer
+
+        self.compiled = True
+
+    def load_weights(self, filepath):
+        filename, extension = os.path.splitext(filepath)
+        actor_filepath = filename + '_actor' + extension
+        critic_filepath = filename + '_critic' + extension
+        self.actor.load_weights(actor_filepath)
+        self.critic.load_weights(critic_filepath)
+        self.update_target_models_hard()
+
+    def save_weights(self, filepath, overwrite=False):
+        filename, extension = os.path.splitext(filepath)
+        actor_filepath = filename + '_actor' + extension
+        critic_filepath = filename + '_critic' + extension
+        self.actor.save_weights(actor_filepath, overwrite=overwrite)
+        self.critic.save_weights(critic_filepath, overwrite=overwrite)
+
+    def update_target_models_hard(self):
+        self.target_critic.set_weights(self.critic.get_weights())
+        self.target_actor.set_weights(self.actor.get_weights())
+
+    # TODO: implement pickle
+
+    def reset_states(self):
+        if self.random_process is not None:
+            self.random_process.reset_states()
+        self.recent_action = None
+        self.recent_observation = None
+        if self.compiled:
+            self.actor.reset_states()
+            self.critic.reset_states()
+            self.target_actor.reset_states()
+            self.target_critic.reset_states()
+
+    def process_state_batch(self, batch):
+        batch = np.array(batch)
+        if self.processor is None:
+            return batch
+        return self.processor.process_state_batch(batch)
+
+    def select_action(self, state):
+        batch = self.process_state_batch([state])
+        action = self.actor.predict_on_batch(batch).flatten()
+        assert action.shape == (self.nb_actions,)
+
+        # Apply noise, if a random process is set.
+        if self.training and self.random_process is not None:
+            noise = self.random_process.sample()
+            assert noise.shape == action.shape
+            action += noise
+        if self.currentStep % 10000 == 0:
+            print("e:", self.e * (1- self.currentStep/self.steps))
+        if random.random() < self.e * (1- self.currentStep/self.steps):
+            self.currentStep += 1
+            action = self.env.action_space.sample()
+        return action
+
+    def forward(self, observation):
+        # Select an action.
+        state = self.memory.get_recent_state(observation)
+        action = self.select_action(state)  # TODO: move this into policy
+
+        # Book-keeping.
+        self.recent_observation = observation
+        self.recent_action = action
+
+        return action
+
+    @property
+    def layers(self):
+        return self.actor.layers[:] + self.critic.layers[:]
+
+    @property
+    def metrics_names(self):
+        names = self.critic.metrics_names[:]
+        if self.processor is not None:
+            names += self.processor.metrics_names[:]
+        return names
+
+    def backward(self, reward, terminal=False):
+        # Store most recent experience in memory.
+        if self.step % self.memory_interval == 0:
+            self.memory.append(self.recent_observation, self.recent_action, reward, terminal,
+                               training=self.training)
+
+        metrics = [np.nan for _ in self.metrics_names]
+        if not self.training:
+            # We're done here. No need to update the experience memory since we only use the working
+            # memory to obtain the state over the most recent observations.
+            return metrics
+
+        # Train the network on a single stochastic batch.
+        can_train_either = self.step > self.nb_steps_warmup_critic or self.step > self.nb_steps_warmup_actor
+        if can_train_either and self.step % self.train_interval == 0:
+            experiences = self.memory.sample(self.batch_size)
+            assert len(experiences) == self.batch_size
+
+            # Start by extracting the necessary parameters (we use a vectorized implementation).
+            state0_batch = []
+            reward_batch = []
+            action_batch = []
+            terminal1_batch = []
+            state1_batch = []
+            for e in experiences:
+                state0_batch.append(e.state0)
+                state1_batch.append(e.state1)
+                reward_batch.append(e.reward)
+                action_batch.append(e.action)
+                terminal1_batch.append(0. if e.terminal1 else 1.)
+
+            # Prepare and validate parameters.
+            state0_batch = self.process_state_batch(state0_batch)
+            state1_batch = self.process_state_batch(state1_batch)
+            terminal1_batch = np.array(terminal1_batch)
+            reward_batch = np.array(reward_batch)
+            action_batch = np.array(action_batch)
+            assert reward_batch.shape == (self.batch_size,)
+            assert terminal1_batch.shape == reward_batch.shape
+            assert action_batch.shape == (self.batch_size, self.nb_actions)
+
+            # Update critic, if warm up is over.
+            if self.step > self.nb_steps_warmup_critic:
+                target_actions = self.target_actor.predict_on_batch(state1_batch)
+                assert target_actions.shape == (self.batch_size, self.nb_actions)
+                if len(self.critic.inputs) >= 3:
+                    state1_batch_with_action = state1_batch[:]
+                else:
+                    state1_batch_with_action = [state1_batch]
+                state1_batch_with_action.insert(self.critic_action_input_idx, target_actions)
+                target_q_values = self.target_critic.predict_on_batch(state1_batch_with_action).flatten()
+                assert target_q_values.shape == (self.batch_size,)
+
+                # Compute r_t + gamma * max_a Q(s_t+1, a) and update the target ys accordingly,
+                # but only for the affected output units (as given by action_batch).
+                discounted_reward_batch = self.gamma * target_q_values
+                discounted_reward_batch *= terminal1_batch
+                assert discounted_reward_batch.shape == reward_batch.shape
+                targets = (reward_batch + discounted_reward_batch).reshape(self.batch_size, 1)
+
+                # Perform a single batch update on the critic network.
+                if len(self.critic.inputs) >= 3:
+                    state0_batch_with_action = state0_batch[:]
+                else:
+                    state0_batch_with_action = [state0_batch]
+                state0_batch_with_action.insert(self.critic_action_input_idx, action_batch)
+                metrics = self.critic.train_on_batch(state0_batch_with_action, targets)
+                if self.processor is not None:
+                    metrics += self.processor.metrics
+
+            # Update actor, if warm up is over.
+            if self.step > self.nb_steps_warmup_actor:
+                # TODO: implement metrics for actor
+                if len(self.actor.inputs) >= 2:
+                    inputs = state0_batch[:]
+                else:
+                    inputs = [state0_batch]
+                if self.uses_learning_phase:
+                    inputs += [self.training]
+                action_values = self.actor_train_fn(inputs)[0]
+                assert action_values.shape == (self.batch_size, self.nb_actions)
+
+        if self.target_model_update >= 1 and self.step % self.target_model_update == 0:
+            self.update_target_models_hard()
+
+        return metrics
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ENV_NAME = 'BipedalWalker-v2'
+#gym.undo_logger_setup()
+
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+assert len(env.action_space.shape) == 1
+nb_actions = env.action_space.shape[0]
+
+# Next, we build a very simple model.
+actor = Sequential()
+actor.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+actor.add(Dense(100))
+actor.add(Activation('relu'))
+actor.add(Dense(100))
+actor.add(Activation('relu'))
+actor.add(Dense(50))
+actor.add(Activation('relu'))
+actor.add(Dense(nb_actions))
+actor.add(Activation('relu'))
+print(actor.summary())
+
+action_input = Input(shape=(nb_actions,), name='action_input')
+observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
+flattened_observation = Flatten()(observation_input)
+x = Concatenate()([action_input, flattened_observation])
+x = Dense(100)(x)
+x = Activation('relu')(x)
+x = Dense(100)(x)
+x = Activation('relu')(x)
+x = Dense(100)(x)
+x = Activation('relu')(x)
+x = Dense(1)(x)
+x = Activation('relu')(x)
+critic = Model(inputs=[action_input, observation_input], outputs=x)
+print(critic.summary())
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = SequentialMemory(limit=500000, window_length=1)
+
+#random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.5,sigma_min = .2)
+agent = DDPGAgent(env = env, nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
+                  memory=memory, gamma=.99,steps = 5000000)
+agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+agent.load_weights('model_500000/ddpg_{}_weights.h5f'.format(ENV_NAME))
+agent.fit(env, nb_steps=5000000, visualize=False, verbose=1, nb_max_episode_steps=1000)
+
+# After training is done, we save the final weights.
+agent.save_weights('model_500000/ddpg_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+agent.test(env, nb_episodes=5, visualize=False, nb_max_episode_steps=500)
---
+++ Testing/DQN.py
@@ -0,0 +1,175 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# import os
+
+# def installWrapper():
+#     # Install a Drive FUSE wrapper.
+#     # https://github.com/astrada/google-drive-ocamlfuse
+#     !apt-get update -qq 2>&1 > /dev/null
+#     !apt-get install -y -qq software-properties-common python-software-properties module-init-tools
+#     !add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+#     !apt-get update -qq 2>&1 > /dev/null
+#     !apt-get -y install -qq google-drive-ocamlfuse fuse
+    
+# def auth():
+#     from google.colab import auth
+#     auth.authenticate_user()
+    
+# def authorizeWrapper():
+#     # Generate creds for the Drive FUSE library.
+#     from google.colab import output
+#     from oauth2client.client import GoogleCredentials
+#     import time
+#     creds = GoogleCredentials.get_application_default()
+#     import getpass
+#     # Determine if Drive Fuse credential setup is already complete.
+#     fuse_credentials_configured = False
+#     with output.temporary():
+#       !google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1
+#       # _exit_code is set to the result of the last "!" command.
+#       fuse_credentials_configured = _exit_code == 0
+#     # Sleep for a short period to ensure that the previous output has been cleared.
+#     time.sleep(1)
+#     if fuse_credentials_configured:
+#       print('Drive FUSE credentials already configured!')
+#     else:
+#       # Work around misordering of STREAM and STDIN in Jupyter.
+#       # https://github.com/jupyter/notebook/issues/3159
+#       prompt = !google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL
+#       vcode = getpass.getpass(prompt[0] + '\n\nEnter verification code: ')
+#       !echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}
+    
+# def createDriveDir():
+#     !mkdir -p drive
+#     !google-drive-ocamlfuse drive
+    
+# installWrapper()
+# auth()
+# authorizeWrapper()
+# createDriveDir()
+
+
+# In[ ]:
+
+
+import random
+import gym
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import sgd,Adam
+from itertools import product as possibleIterations
+import copy
+
+
+# In[ ]:
+
+
+class DeepQAgent:
+    def __init__(self, state_size, action_space):
+        self.state_size = state_size
+        self.action_size = 81 # todo check, looks shady
+        self.memory = deque(maxlen=200000) # chaged this from 2000
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.05 # changed this from 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.action_space = actions_space
+
+    def get_action_from_prediction(self, predict):
+        return self.action_space[np.argmax(predict[0])]
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(100, input_dim=self.state_size, activation='relu')) # changed layer count from 24
+        model.add(Dense(100, activation='relu'))
+        model.add(Dense(self.action_size, activation='relu')) # changed this from linear
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        action_index = self.action_space.tolist().index(action.tolist())
+        self.memory.append((state, action_index, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return self.action_space[np.random.choice([i for i in range(len(self.action_space))])]
+        act_values = self.model.predict(state) # what does this return
+        return self.get_action_from_prediction(act_values)
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0])) # Returns q-values
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+
+# In[ ]:
+
+
+EPISODES = 1000
+
+def get_actions():
+    possibleTorques = np.array([-1.0, 0.0, 1.0])
+    possibleActions = np.array(list(possibleIterations(possibleTorques, possibleTorques, possibleTorques, possibleTorques)))
+    return possibleActions
+
+if __name__ == "__main__":
+    env = gym.make('BipedalWalker-v2')
+    state_size = env.observation_space.shape[0]
+    actions_space = get_actions()
+    action_size = len(actions_space)
+    agent = DeepQAgent(state_size, actions_space)
+    # agent2 = DeepQAgent(state_size, actions_space)
+    done = False
+    batch_size = 32
+    c = 0
+    recent_average = deque(maxlen = 100)
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        total_reward = 0
+        for time in range(500):
+            c += 1
+            #env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            total_reward += reward
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, total_reward, agent.epsilon))
+                recent_average.append(total_reward)
+                print("c = ", c, " Recent Average = ", sum(recent_average)/len(recent_average))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+            # if c >= 1000:
+            #     print('updating model')
+            #     c = 0
+            #     # agent2 = copy.deepcopy(agent)
+            #     agent2.model.set_weights(agent.model.get_weights())
+        if e % 50 == 0:
+            agent.save("Bipedal-dqn-testing.h5")
+
